1851c618b0d74e15e69000d50e99232bacf2fe0d
danwritecode <dnelson4993@outlook.com>
2023-05-22 15:58:01 -0400

    fixed bug in binary search

--- grokking_algorithms/ch1/src/main.rs
+++ grokking_algorithms/ch1/src/main.rs
@@ -16,6 +16,10 @@ fn binary_search(search_val: i32, list: Vec<i32>) -> i32 {
             return mid_index as i32;
         }
 
+        if mid_index == 0 {
+            return -1;
+        }
+
         // if guess is greater than the search val, then set high index to mid -1
         if guess > search_val {
             high_index = mid_index - 1; // don't need to include mid since we checked it with guess
@@ -46,17 +50,17 @@ mod tests {
           assert_eq!(1,  binary_search(3, vec![1, 3, 5]));
           assert_eq!(2,  binary_search(5, vec![1, 3, 5]));
           assert_eq!(-1, binary_search(0, vec![1, 3, 5]));
-          // assert_eq!(-1, binary_search(2, vec![1, 3, 5]));
-          // assert_eq!(-1, binary_search(4, vec![1, 3, 5]));
-          // assert_eq!(-1, binary_search(6, vec![1, 3, 5]));
-          // assert_eq!(0,  binary_search(1, vec![1, 3, 5, 7]));
-          // assert_eq!(1,  binary_search(3, vec![1, 3, 5, 7]));
-          // assert_eq!(2,  binary_search(5, vec![1, 3, 5, 7]));
-          // assert_eq!(3,  binary_search(7, vec![1, 3, 5, 7]));
-          // assert_eq!(-1, binary_search(0, vec![1, 3, 5, 7]));
-          // assert_eq!(-1, binary_search(2, vec![1, 3, 5, 7]));
-          // assert_eq!(-1, binary_search(4, vec![1, 3, 5, 7]));
-          // assert_eq!(-1, binary_search(6, vec![1, 3, 5, 7]));
-          // assert_eq!(-1, binary_search(8, vec![1, 3, 5, 7]));
+          assert_eq!(-1, binary_search(2, vec![1, 3, 5]));
+          assert_eq!(-1, binary_search(4, vec![1, 3, 5]));
+          assert_eq!(-1, binary_search(6, vec![1, 3, 5]));
+          assert_eq!(0,  binary_search(1, vec![1, 3, 5, 7]));
+          assert_eq!(1,  binary_search(3, vec![1, 3, 5, 7]));
+          assert_eq!(2,  binary_search(5, vec![1, 3, 5, 7]));
+          assert_eq!(3,  binary_search(7, vec![1, 3, 5, 7]));
+          assert_eq!(-1, binary_search(0, vec![1, 3, 5, 7]));
+          assert_eq!(-1, binary_search(2, vec![1, 3, 5, 7]));
+          assert_eq!(-1, binary_search(4, vec![1, 3, 5, 7]));
+          assert_eq!(-1, binary_search(6, vec![1, 3, 5, 7]));
+          assert_eq!(-1, binary_search(8, vec![1, 3, 5, 7]));
     }
 }
